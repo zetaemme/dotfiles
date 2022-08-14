58aa49cfec12a49224ef7cc867883533b4093602
Mattia Zorzan <mattia.zorzan@gmail.com>
2022-08-14 16:36:10 +0200

    Removed useless script

--- mirrorlist/.get_updated_mirrorlist.sh
+++
@@ -1,6 +0,0 @@
-# /bin/bash
-
-wget -O mirrorlist "https://www.archlinux.org/mirrorlist/?country=DE&country=IT&protocol=https&ip_version=4"
-sed -ie 's/^.//g' ./mirrorlist
-sudo mv ./mirrorlist /etc/pacman.d/mirrorlist
-sudo rm -rf ./mirrorliste
